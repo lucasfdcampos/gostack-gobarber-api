a94215d20b5fd41f330c92ab11ea4198f78c676c
lucasfdcampos <lucascampos.ads@gmail.com>
2020-08-30 17:20:15 -0300

    Deploy: Production configuration

---
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProvidersController from '../controllers/ProvidersController';
+import ProviderMOnthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+
+const providersRouter = Router();
+const providersController = new ProvidersController();
+const providerMOnthAvailabilityController = new ProviderMOnthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+
+providersRouter.use(ensureAuthenticated);
+
+providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
+  providerMOnthAvailabilityController.index);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
+  providerDayAvailabilityController.index);
+
+export default providersRouter;
---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,56 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from './AuthenticateUserService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashprovider: FakeHashProvider;
+let authenticateUser: AuthenticateUserService;
+
+describe('AuthenticateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashprovider = new FakeHashProvider();
+
+    authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashprovider);
+  });
+
+  it('should be a able to authenticate', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    await expect(authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be a able to authenticate with wrong password', async () => {
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    await expect(authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: 'wrong-password',
+    })).rejects.toBeInstanceOf(AppError)
+  });
+});
